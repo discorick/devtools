a29497084106c5cb20333eaa87d1529be876f973
Richard Frank <richardfrank@me.com>
2012-11-20 10:29:19 -0700

    Fixed bad spec_helper glob

--- ubuntu/scripts/backup_current/backup_now.rb
+++ ubuntu/scripts/backup_current/backup_now.rb
@@ -1,11 +1,5 @@
 #Script to Backup the contents of the Home Folder
 #Prompts for addition locations for backup
 
-class NewClass
-
-end
-
-class TwoClass
-end
 
 
--- ubuntu/spec/scripts/backup_now_spec.rb
+++ ubuntu/spec/scripts/backup_now_spec.rb
@@ -2,11 +2,6 @@ require_relative "../spec_helper.rb"
 #Specs for Backup
 
 describe "A Backup System" do
-  it "Backs shiiit up" do
-  end
-  
-  it "new test" do
-  end
 end
 
  
