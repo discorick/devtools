41a777057ff346f5b6e445fe9b87163f75fda645
Richard Frank <richardfrank@me.com>
2013-01-09 16:35:47 -0700

    Playing with different approaches to matching

--- ubuntu/lib/strip_from_array.rb
+++ ubuntu/lib/strip_from_array.rb
@@ -1,7 +1,7 @@
 require_relative '../components.rb'
 
 class Strip
-  def self.matching_elements_from_array(array, ignore_list)
+  def self.disablematching_elements_from_array(array, ignore_list)
     array.delete_if do |element|
       ignore_list.include? element
     end
@@ -11,6 +11,14 @@ class Strip
     hash.send(each_key_or_value) do |array|
       array.delete_if do |element|
         ignore_list.include? element
+      end
+    end
+  end
+
+  def self.matching_elements_from_array(array, ignore_list)
+    array.delete_if do |element|
+      ignore_list.find do |matcher| #what would I do if there was no find method ?
+        matcher =~ element
       end
     end
   end
--- ubuntu/spec/specs/strip_from_array_spec.rb
+++ ubuntu/spec/specs/strip_from_array_spec.rb
@@ -4,8 +4,8 @@ require_relative "../../components.rb"
 describe Strip do
   context "\n - When Stripping an array of elements containing X number of matching elements" do
     it "\n - Removes the items matching the ignore list from an Array" do
-      ignore_list = ["git",".thisfile",".alsothisfile"]
-      array_to_strip = ["git",".thisfile",".alsothisfile", ".notthisfile", "orthisone"]
+      ignore_list = ["git",".thisfile",".alsothisfile", /hello/]
+      array_to_strip = ["git",".thisfile",".alsothisfile", ".notthisfile", "orthisone", "hello"]
       Strip.matching_elements_from_array(array_to_strip, ignore_list)
       array_to_strip.should == [".notthisfile", "orthisone"]
     end
@@ -15,6 +15,13 @@ describe Strip do
       array_to_strip_from_hash = {"hash" => ["git",".thisfile",".alsothisfile", ".notthisfile", "orthisone"]}
       Strip.matching_elements_from_hash(array_to_strip_from_hash,:each_value, ignore_list)
       array_to_strip_from_hash.should == {"hash" => [".notthisfile", "orthisone"]}
+    end
+
+    it "\n - Removes the items matching the ignore list from an Array" do
+      ignore_list = [/git/,/\.thisfile/,/\.alsothisfile/]
+      array_to_strip = ["git",".thisfile",".alsothisfile", ".notthisfile", "orthisone", "hello"]
+      Strip.matching_elements_from_array(array_to_strip, ignore_list)
+      array_to_strip.should == [".notthisfile", "orthisone", "hello"]
     end
   end
 end
