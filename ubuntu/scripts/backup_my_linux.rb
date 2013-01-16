0cc1f8ec32dc3df9b03859b4521b1f31a67fed6f
Richard Frank <richardfrank@me.com>
2013-01-16 15:15:39 -0700

    Simple Backup Script

--- ubuntu/scripts/backup_my_linux.rb
+++ ubuntu/scripts/backup_my_linux.rb
@@ -1,5 +1,15 @@
 require_relative '../components.rb'
 
+#notes.. 
+#- Would you like to backup up home?
+#- Full or Recursive? (Warning! Recursive will backup ALL file and directories in current user
+
+def enter_to_continue
+  puts "Push enter to continue" 
+  continue = gets
+  exit if continue.chomp == 'exit'
+end
+
 class TimeThis 
   def self.now &block
     start = Time.now
@@ -10,24 +20,48 @@ end
 
 GetEnvVariables.kick_off
 backup = Backuperator.new_backup
-
 eval File.read('resources/backup_list.rb')
-abort("Warning! Cannot load 'resources/backup_list.rb' ... Aborting Backup") unless $backup_directory_list.length > 0
+puts "If you have not yet added the custom folders to backup or populated the ignore lists \nplease type 'exit' to do this now otherwise press any key: \n \n"
+enter_to_continue
+system("clear")
 
-puts "Preparing File Lists: \n \n"
-$backup_directory_list.each{|saved_dir| backup.add_all_directories(saved_dir)}
+puts "Backup Files in Home Folder? (y/n) \n \n"
+confirm = gets.chomp
+if confirm == 'y'
+  puts "\n1 Recursive \n2 Non-Recursive \n"
+  puts "Warning!!! Recursive will copy ALL file AND directory contents of #{configatron.user}"
+  $recursive_option = gets.to_i
+  backup.add_all_directories("#{configatron.user_path}") if $recursive_option == 1
+  backup.add_directory("#{configatron.user_path}") if $recursive_option == 2
+end
+system("clear")
 
+puts "Backup Other Folders? (y/n) \nBe Aware you are already doing a FULL backup of this account" if $recursive_option == 1
+puts "Backup Other Folders? (y/n)" if $recursive_option != 1
+confirm2 = gets.chomp
+if confirm2 == "y"
+  $backup_directory_list.each{|saved_dir| backup.add_all_directories(saved_dir)}
+end
 Strip.matching_elements_from_hash(configatron.file_backup_list,:each_key,$ignore_directories)
+system("clear")
 
+puts "No Folders Have Been Specified! Exiting..." if configatron.file_backup_list.length == 0 
+exit if configatron.file_backup_list.length == 0 
+
+puts "Folders to Backup: \n \n"
 configatron.file_backup_list.each_key{|dir|puts dir}
+enter_to_continue
+system("clear")
+
+puts "\n \nPreparing File Lists: \n \n"
 backup.build_file_lists
 backup.make_file_lists_expandable
-
 Strip.matching_elements_from_hash(configatron.file_backup_list,:each_value,$ignore_files)
-
 
 puts "Backup to .... ?"
 destination = gets.chomp
-backup.expand_to destination
-puts "Backup Finished"
+TimeThis.now do
+  backup.expand_to destination
+  puts "Backup Finished"
+end
 
