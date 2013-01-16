fccb84a9635376e89b90dd279d676e8e54d33f8b
Richard Frank <richardfrank@me.com>
2013-01-16 10:05:26 -0700

    Changed File name to reflect accuracy

--- ubuntu/components.rb
+++ ubuntu/components.rb
@@ -1,5 +1,5 @@
 require 'configatron'
 
+require_relative 'lib/strip_matching_elements.rb'
 require_relative 'lib/backuperator.rb'
-require_relative 'lib/strip_from_array.rb'
 require_relative 'lib/gather_environment.rb'
