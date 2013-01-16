d5ac4d3bf75c34a230dbcf39649a78e8ce069618
Richard Frank <richardfrank@me.com>
2013-01-16 11:21:24 -0700

    Save

--- ubuntu/lib/strip_matching_elements.rb
+++ ubuntu/lib/strip_matching_elements.rb
@@ -1,16 +1,13 @@
 require_relative '../components.rb'
 
 class Strip
-  def self.disablematching_elements_from_array(array, ignore_list)
-    array.delete_if do |element|
-      ignore_list.include? element
-    end
-  end
 
   def self.matching_elements_from_hash(hash,each_key_or_value,ignore_list)
-    hash.send(each_key_or_value) do |array|
-      array.delete_if do |element|
-        ignore_list.include? element
+    hash.send(each_key_or_value) do |list|
+      list.delete_if do |element|
+        ignore_list.find do |matcher| 
+          matcher =~ element
+        end
       end
     end
   end
@@ -24,5 +21,4 @@ class Strip
   end
 
 end
-
 
